e76ca0c5274143149e20a48c30d65c76d6775d6c
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-02-02 11:14:09 -0500

    Initial Commit

---
+++ CarTradingCards/CarTradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTradingCardsApp.swift
+//  CarTradingCards
+//
+//  Created by Sebastian Frosch on 02.02.23.
+//
+
+import SwiftUI
+
+@main
+struct CarTradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
